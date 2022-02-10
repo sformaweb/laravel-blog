3e8365d32419d52fc1a2451d18092e8feec63d6c
sformaweb <sara.formaweb@gmail.com>
2022-02-10 14:37:30 +0100

    10-feb

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>Posts</title>
+</head>
+
+<body>
+
+    <h1>Páxina de publicacións</h1>
+    <article> 
+    <a href="../posts/primeiro-post.html"> <h2>Primeira publicación</h2></a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugit nulla officiis cumque iste optio maiores, ex recusandae alias in iure autem itaque earum tempore minima dolorem voluptate laudantium aspernatur.</p>
+    </article>
+
+    <article>
+    <a href="/posts"> <h2>Segunda publicación</h2></a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugit nulla officiis cumque iste optio maiores, ex recusandae alias in iure autem itaque earum tempore minima dolorem voluptate laudantium aspernatur.</p>
+    </article>
+
+    <article>
+    <a href="post"> <h2>Terceira publicación</h2></a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugit nulla officiis cumque iste optio maiores, ex recusandae alias in iure autem itaque earum tempore minima dolorem voluptate laudantium aspernatur.</p>
+    </article>
+
+</body>
+
+</html>
