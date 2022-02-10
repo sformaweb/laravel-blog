d454b2b8a00c22bebd01b91e751119b319d340d5
sformaweb <sara.formaweb@gmail.com>
2022-02-10 14:37:48 +0100

    10-feb

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>Posts</title>
+</head>
+
+<body>
+
+    <h1>Publicación #1</h1>
+    <article> 
+    <a href="post"> <h2>Primeira publicación</h2></a>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis fugit nulla officiis cumque iste optio maiores, ex recusandae alias in iure autem itaque earum tempore minima dolorem voluptate laudantium aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ipsum vero repellat mollitia architecto, ullam modi deleniti provident nisi dolores numquam eveniet accusamus accusantium deserunt eaque quia repellendus, ut esse. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, distinctio esse fuga pariatur porro saepe velit ad molestiae quidem, ipsum voluptatem eaque voluptate alias magnam officiis consectetur nam eos a!</p>
+    
+    </article>
+<a href="posts.blade.php">Ir atrás</a>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts.blade.php', function () {
+    return view('posts');
+});
+
+Route::get('/posts{post}', function ($slug) {
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    if(!file_exists($path)){
+        ddd('o arquivo non existe');
+// return redirect('/');
+    }
+
+$post = file_get_contents($path);
+
+    return view('post',[
+        'post' => $post
+    ]);
+
+
+});
+
